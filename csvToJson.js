1eba22ac452c52fc8a42008182b009f98757f81d
cyrine <cyrine.rihane@gmail.com>
2017-12-06 16:15:11 +0100

    js file updated

--- csvToJson.js
+++ csvToJson.js
@@ -1,22 +1,23 @@
 const fs = require('fs')
-let csv = fs.readFileSync(process.argv[2]).toString();
+let csv = fs.readFileSync(process.argv[2]).toString(); //read the file and convert from buffer to string
+
 function csvJSON(csv){
-  let lines = csv.split("\n");
-  let keys = lines[0].split(",");
-  let result = [];
+    let lines = csv.split("\n"); //tab of every line
+    let keys = lines[0].split(","); //Tab of keys "id,firstname ..."
+    let result = []; //The result tab
 
-  for(var i=1 ;i < lines.length - 1;i++){
-     let obj = {};
-     let currentline = lines[i].split(",");
+    for(var i=1 ;i < lines.length - 1;i++){
+       let obj = {};
+       let currentline = lines[i].split(",");
 
-     for(var j=0; j < keys.length;j++){
-       obj[keys[j]] = currentline[j];
+       for(var j=0; j < keys.length;j++){
+         obj[keys[j]] = currentline[j];
+       }
+       result.push(obj);
+
      }
-     result.push(obj);
 
-   }
-
-  return JSON.stringify(result);
+    return JSON.stringify(result);
 }
-fs.writeFileSync('result.json',result)
+fs.writeFileSync('result.json',result) //put the result into a json file
 console.log(csvJSON(csv))
